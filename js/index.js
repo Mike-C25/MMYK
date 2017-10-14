93c23d85fe0039e241203aa7d1509cab2f663447
Michael Chiang <michael.chiang25@gmail.com>
2017-10-14 18:01:36 -0400

    index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
-var request = require('superagent');
+// var request = require('superagent');
 
 $(document).ready(function() {
 
-    function getUserInfo() {
-        request
-	        .get('http://api.reimaginebanking.com/accounts?key=e37a0183d9b8d2e386401e45ec9dab74')
-	        .then(function(res) {
-	            // console.log(res);
-	            console.log(res.status, res.body);
-	        });
-    }
+    // function getUserInfo() {
+    //     request
+    //         .get('http://api.reimaginebanking.com/accounts?key=e37a0183d9b8d2e386401e45ec9dab74')
+    //         .then(function(res) {
+    //             // console.log(res);
+    //             console.log(res.status, res.body);
+    //         });
+    // }
+
+    $.ajax({
+        url: 'api.reimaginebanking.com/accounts?key=e37a0183d9b8d2e386401e45ec9dab74',
+        success: function(results) {
+            console.log(results);
+        }
+    });
 
     getUserInfo();
 
