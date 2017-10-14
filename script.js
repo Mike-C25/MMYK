657716fd8f53ce6db7ed820917213444b88c75d8
Michael Chiang <michael.chiang25@gmail.com>
2017-10-14 14:03:13 -0400

    Added promise function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var request = require('superagent');
-request.get('http://api.reimaginebanking.com/atms?key=e37a0183d9b8d2e386401e45ec9dab74').end(function(res){
+
+
+
+
+request.get('http://api.reimaginebanking.com/accounts?key=e37a0183d9b8d2e386401e45ec9dab74').then(function(res){
+	console.log(res.status, res.body);
     statusGet(res.status);
     resGet(res.body); //do something
 });
 
 
 function statusGet(status){
-	console.log(status);
+	console.log(typeof(status) );
 }
 
 
