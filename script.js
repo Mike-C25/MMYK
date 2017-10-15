48c0399f9c62db9090e8bb0c27fe8ce0c4111ec8
KBerry4492 <kmb23@comcast.net>
2017-10-14 22:09:13 -0400

    charttester

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var request = require('superagent');
 
+accountKey = "59e23357a73e4942cdafe22d";
+//http://api.reimaginebanking.com/accounts?key=e37a0183d9b8d2e386401e45ec9dab74
 
-
-
-request.get('http://api.reimaginebanking.com/accounts?key=e37a0183d9b8d2e386401e45ec9dab74').then(function(res){
-	console.log(res.status, res.body);
-    statusGet(res.status);
-    resGet(res.body); //do something
+request.get('http://api.reimaginebanking.com/accounts/59e23357a73e4942cdafe22d/deposits?key=e37a0183d9b8d2e386401e45ec9dab74').then(function(res){
+	// console.log(res.status, res.body);
+ 	// var ret = res.body[0]._id;
+ 	var ret2 = res.body;
+    // statusGet(res.status);
+    // resGet(ret, ret2); //do something
+	// console.log(ret);
+	console.log(ret2);
 });
 
 
@@ -15,6 +19,15 @@ function statusGet(status){
 }
 
 
-function resGet(response){
-	console.log(response);
+function resGet(resId, resId2){
+	console.log(resId+" , "+resId2);
+	console.log("resID: "+resId);
+	request.get('http://api.reimaginebanking.com/accounts/'+resId+'/deposits?key=e37a0183d9b8d2e386401e45ec9dab74').then(function(res){
+		console.log(JSON.stringify(res.body,null,2));
+	})
 }
+
+//verfy user
+//collect account id
+// collect purchase data
+//
